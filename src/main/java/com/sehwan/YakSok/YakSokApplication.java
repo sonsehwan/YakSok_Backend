0409790e00799a4b9a5df6ad13fa63b52525bcc9
Son Sehwan <sahwan45@naver.com>
2026-03-26 00:49:20 +0900

    푸시 확인용

--- src/main/java/com/sehwan/YakSok/YakSokApplication.java
+++ src/main/java/com/sehwan/YakSok/YakSokApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YakSokApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(YakSokApplication.class, args);
     }
-
 }
