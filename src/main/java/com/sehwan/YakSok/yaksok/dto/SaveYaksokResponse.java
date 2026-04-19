fe1faf7a2de258236dbdb1b3de1a73e80f23d325
sahwan <sahwan458@gmail.com>
2026-04-19 20:49:34 +0900

    [fix] @Getter 추가

--- src/main/java/com/sehwan/YakSok/yaksok/dto/SaveYaksokResponse.java
+++ src/main/java/com/sehwan/YakSok/yaksok/dto/SaveYaksokResponse.java
@@ -2,11 +2,13 @@ package com.sehwan.YakSok.yaksok.dto;
 
 import com.sehwan.YakSok.yaksok.entity.Notification;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Setter
 @AllArgsConstructor
 public class SaveYaksokResponse {
