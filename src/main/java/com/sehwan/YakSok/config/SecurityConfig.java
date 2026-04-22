c116c7816d8cff20b8fc7d8d5e9ea30c247bede3
sahwan <sahwan458@gmail.com>
2026-04-22 16:28:02 +0900

    [feat] 알림 복용 상태 변경 api 권한 부여

--- src/main/java/com/sehwan/YakSok/config/SecurityConfig.java
+++ src/main/java/com/sehwan/YakSok/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/users/password").permitAll()
                         .requestMatchers("/api/medicine/search").permitAll()
                         .requestMatchers("/api/yaksok").permitAll()
+                        .requestMatchers("/notifications/{notificationId}/status").permitAll()
 
                         .requestMatchers("/error").permitAll()
 
