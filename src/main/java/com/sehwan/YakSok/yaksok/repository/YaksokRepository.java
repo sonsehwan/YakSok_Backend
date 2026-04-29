c77ad500af83cf423eeafdea9e29394fb0e5b7dc
sahwan <sahwan458@gmail.com>
2026-04-30 07:04:11 +0900

    [fix] yaksok 리스트 시작 날짜 오름차순으로 전달하게 수정

--- src/main/java/com/sehwan/YakSok/yaksok/repository/YaksokRepository.java
+++ src/main/java/com/sehwan/YakSok/yaksok/repository/YaksokRepository.java
@@ -2,9 +2,14 @@ package com.sehwan.YakSok.yaksok.repository;
 
 import com.sehwan.YakSok.yaksok.entity.Yaksok;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface YaksokRepository extends JpaRepository<Yaksok, Long> {
+
+    @Query("SELECT y FROM Yaksok y " +
+            "WHERE y.user.email = :email " +
+            "ORDER BY y.startDate ASC")
     List<Yaksok> findAllByUserEmail(String email);
 }
