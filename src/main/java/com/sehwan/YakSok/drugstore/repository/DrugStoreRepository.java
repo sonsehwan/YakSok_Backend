7ab4ea659d77b8f63f5b0f00d131f30305cc70bf
sahwan <sahwan458@gmail.com>
2026-06-17 21:34:05 +0900

    [feat] hpid로 데이터가 존재하는지 확인

--- src/main/java/com/sehwan/YakSok/drugstore/repository/DrugStoreRepository.java
+++ src/main/java/com/sehwan/YakSok/drugstore/repository/DrugStoreRepository.java
@@ -4,4 +4,5 @@ import com.sehwan.YakSok.drugstore.entity.DrugStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DrugStoreRepository extends JpaRepository<DrugStore, Long> {
+    boolean existsByHpid(String hpid);
 }
