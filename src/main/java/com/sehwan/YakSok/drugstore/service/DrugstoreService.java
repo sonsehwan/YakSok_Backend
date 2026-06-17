04989d30d9fa75aafdff4eba6633e1a9c5db6333
sahwan <sahwan458@gmail.com>
2026-06-17 22:09:34 +0900

    [feat] 트랜젝션 추가

--- src/main/java/com/sehwan/YakSok/drugstore/service/DrugstoreService.java
+++ src/main/java/com/sehwan/YakSok/drugstore/service/DrugstoreService.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
@@ -33,7 +34,7 @@ public class DrugstoreService {
     @Value("${api.medicine.service-key}")
     private String serviceKey;
 
-
+    @Transactional
     public List<DrugStoreDto> getCloseDrugStoreList(String latitude, String longitude, int page) {
         return callApi(latitude, longitude, page, 20);
     }
